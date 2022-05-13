89d4e4f8b6ea7eb52b002e65dc17ecb62df67d1b
Thomas Dimperio <twdimperio@gmail.com>
2022-05-12 19:23:01 -0600

    add solutions

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,20 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const uniques = new Set();
+
+  for (let v of arr) {
+    if (uniques.has(v)) {
+      return v;
+    }
+    uniques.add(v);
+  }
+  return -1;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  
+  console.log("Expecting: 5");
+  console.log("=>", findFirstDuplicate([1, 4, 3, 5, 6, 7, 8, 5]));
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -15,5 +26,9 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// Initialize empty set, then loop through the array. If the set contains 
+// the current value, return the current value. Else, return -1.
+
+// Sets only contain unique values. If the set contains the current element
+// of the array, then it is a duplicate. The first duplicate to be found should
+// be returned.
--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,5 +1,16 @@
+require 'set'
+
 def find_first_duplicate(arr)
-  # type your code in here
+  uniques = Set.new
+
+  arr.each do |v|
+    return v if uniques.include?(v)
+
+    uniques.add(v)
+  end
+
+  -1
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +22,13 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
 
-  # Don't forget to add your own!
+  puts "Expecting: 5"
+  puts "=>", find_first_duplicate([1, 4, 5, 3, 5, 8, 7, 6])
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+# Initialize an empty set (sets only contain unique values). Loop through the array
+# checking if the value is in the set. If so, return the value. If no duplicate
+# is found, return -1.
+
+# Sets only contain unique values. If a value in the array is in the set, it 
+# is a duplicate. The first duplicate found should be returned.
